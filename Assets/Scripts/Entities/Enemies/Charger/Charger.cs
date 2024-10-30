01e95631cff9d342ff5176dd49ce0e6c6a749237
John Zhou <chowjohn10@gmail.com>
2024-10-30 18:48:05 -0400

    Charger Idle and Dazed Variables Moved to Charger.cs
    I added the charger idle state and dazed state into the charger class. Now the charger class just needs the other states

--- Assets/Scripts/Entities/Enemies/Charger/Charger.cs
+++ Assets/Scripts/Entities/Enemies/Charger/Charger.cs
@@ -1,4 +1,66 @@
-﻿public class Charger : Enemy
-{
+﻿using UnityEngine;
 
+public class Charger : Enemy
+{
+    [field: Header("Follower: Attack Settings")]
+    [field: SerializeField] public float AttackRange { get; private set; } = 1f;
+    [field: SerializeField] public Vector2Int AttackDamageRange { get; private set; } = new Vector2Int(10, 15);
+    
+
+    [field: Header("Charger: Idle Settings")]
+    [field: SerializeField] public float WanderRadius { get; private set; } = 5f;
+    [field: SerializeField] public float WanderWaitMin { get; private set; } = 1f;
+    [field: SerializeField] public float WanderWaitMax { get; private set; } = 3f;
+
+    [field: Header("Charger: Dazed Settings")]
+    [field: SerializeField] public float DazedDuration { get; private set; } = 5f;
+
+    #region States
+    public ChargerIdleState ChargerIdleState { get; private set; }
+    public ChargerDazedState ChargerDazedState { get; private set; }
+
+    protected override void InitializeStates()
+    {
+        base.InitializeStates();
+
+        ChargerIdleState = new ChargerIdleState(this);
+        ChargerDazedState = new ChargerDazedState(this);
+    }
+    #endregion
+
+    protected override void OnAwake()
+    {
+        base.OnAwake();
+    }
+
+    protected override void OnFixedUpdate()
+    {
+        base.OnFixedUpdate();
+    }
+
+    protected override void OnOnAnimatorMove()
+    {
+        base.OnOnAnimatorMove();
+    }
+
+    protected override void OnOnDisable()
+    {
+        base.OnOnDisable();
+    }
+
+    protected override void OnOnEnable()
+    {
+        //base.OnOnEnable();
+         SetStartState(ChargerIdleState);
+    }
+
+    protected override void OnStart()
+    {
+        base.OnStart();
+    }
+
+    protected override void OnUpdate()
+    {
+        base.OnUpdate();
+    }
 }
---
+++ Assets/Scripts/Entities/Enemies/Charger/States/ChargerIdleState.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ChargerIdleState : EnemyIdleState
+{
+    private Coroutine wanderCoroutine;
+    private Charger charger;
+
+    public ChargerIdleState(Charger enemy) : base(enemy)
+    {
+        charger = enemy;
+    }
+
+    public override void OnEnter()
+    {
+        base.OnEnter();
+        charger.SetSpeedModifier(1f);
+        wanderCoroutine = charger.StartCoroutine(WanderCoroutine());
+        //Debug.Log("Started charger wander coroutine");
+    }
+
+    public override void OnExit()
+    {
+        if (wanderCoroutine != null)
+        {
+            charger.StopCoroutine(wanderCoroutine);
+            wanderCoroutine = null;
+            //Debug.Log("Stopped charger wander coroutine");
+        }
+    }
+
+    public override void Update()
+    {
+        if (charger.Target != null)
+        {
+            //charger.ChangeState(charger.ChargerPlayerDetectedState);
+        }
+    }
+
+
+    public override void FixedUpdate() { }
+
+
+    private IEnumerator WanderCoroutine()
+    {
+        while (charger.CurrentState == this)
+        {
+            yield return new WaitForSeconds(Random.Range(charger.WanderWaitMin, charger.WanderWaitMax));
+            GoToRandomWanderPoint();
+        }
+    }
+
+    private void GoToRandomWanderPoint()
+    {
+        Vector3 randomPoint = (Random.insideUnitSphere * charger.WanderRadius) + charger.transform.position;
+        charger.SetDestination(randomPoint, true);
+    }
+
+}
