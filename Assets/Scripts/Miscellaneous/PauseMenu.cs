244e4bf67babcfe8ae93023986082b626e749105
AFaraazK <92804285+ZaarafA@users.noreply.github.com>
2024-11-05 18:13:25 -0500

    Set up Pause Menu

---
+++ Assets/Scripts/Miscellaneous/PauseMenu.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PauseMenu : MonoBehaviour
+{
+    private GameObject PauseCanvas;
+    private InputAction cameraLook;
+    [SerializeField] private PlayerInput playerInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PauseCanvas = transform.Find("PauseMenu")?.gameObject;
+
+        if (PauseCanvas != null)
+        {
+            PauseCanvas.SetActive(false);
+        }
+        else
+        {
+            Debug.Log("Pause Menu Canvas not Found");
+        }
+    }
+
+    public void TogglePauseMenu()
+    {
+        if (PauseCanvas != null)
+        {
+            // Open Pause Menu
+            if(!PauseCanvas.activeSelf)
+            {
+                PauseCanvas.SetActive(true);
+                Time.timeScale = 0;
+
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+                cameraLook.Disable();
+                Debug.Log("Pause Menu Opened");
+            }
+            else
+            {
+                PauseCanvas.SetActive(false);
+                Time.timeScale = 1;
+
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+                cameraLook.Enable();
+                Debug.Log("Pause Menu Closed");
+            }
+        }
+    }
+
+}
