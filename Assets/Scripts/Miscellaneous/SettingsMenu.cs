aa6eb93f10685117859724c3ad3370b93711af7b
AFaraazK <92804285+ZaarafA@users.noreply.github.com>
2024-11-05 14:29:50 -0500

    Add Options Menu to Unity Input Manager

--- Assets/Scripts/Miscellaneous/SettingsMenu.cs
+++ Assets/Scripts/Miscellaneous/SettingsMenu.cs
@@ -23,30 +23,13 @@ public class SettingsMenu : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if(Input.GetKeyUp(KeyCode.O)) {
-            ToggleMenu();
-        }
-    }
-
     public void ToggleMenu() {
         Debug.Log("MENU OPENED");
         if (SettingsCanvas != null)
         {
-            // Resume
+            // Open Settings Menu
+            // Display Canvas, Freeze Time, Show Cursor, Disable Lookaround
             if (SettingsCanvas.activeSelf)
-            {
-                SettingsCanvas.SetActive(false);
-                Time.timeScale = 1;
-
-                Cursor.lockState = CursorLockMode.Locked;
-                Cursor.visible = false;
-                playerInput.actions.FindActionMap("Gameplay").FindAction("CameraLook").Enable();
-            }
-            // Pause
-            else
             {
                 SettingsCanvas.SetActive(true);
                 Time.timeScale = 0;
@@ -54,6 +37,17 @@ public class SettingsMenu : MonoBehaviour
                 Cursor.lockState = CursorLockMode.None;
                 Cursor.visible = true;
                 playerInput.actions.FindActionMap("Gameplay").FindAction("CameraLook").Disable();
+            }
+            // Close Settings Menu
+            // Hide Canvas, Resume Time, Hide Cursor, Enable Lookaround
+            else
+            {
+                SettingsCanvas.SetActive(false);
+                Time.timeScale = 1;
+
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+                playerInput.actions.FindActionMap("Gameplay").FindAction("CameraLook").Enable();
             }
         }
     }
