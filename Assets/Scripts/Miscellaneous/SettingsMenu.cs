1f87200f7f1347414e15efbe0fcc1a357ce7d482
AFaraazK <92804285+ZaarafA@users.noreply.github.com>
2024-11-05 15:34:11 -0500

    Fullscreen and Vsync + Bugfix

--- Assets/Scripts/Miscellaneous/SettingsMenu.cs
+++ Assets/Scripts/Miscellaneous/SettingsMenu.cs
@@ -2,41 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.InputSystem;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
     private GameObject SettingsCanvas;
+    private InputAction cameraLook;
     [SerializeField] private PlayerInput playerInput;
+    [SerializeField] Toggle fullscreenToggle;
+    [SerializeField] Toggle vsyncToggle;
 
     // Start is called before the first frame update
     void Start()
     {
         SettingsCanvas = transform.Find("SettingsMenu")?.gameObject;
+        cameraLook = playerInput.actions.FindActionMap("Gameplay").FindAction("CameraLook");
 
-        if(SettingsCanvas != null)
+        if (SettingsCanvas != null)
         {
             SettingsCanvas.SetActive(false);
         }
         else
         {
-            Debug.Log("SETTINGS MENU NOT FOUND");
+            Debug.Log("Settings Menu Canvas not Found");
         }
     }
 
     public void ToggleMenu() {
-        Debug.Log("MENU OPENED");
         if (SettingsCanvas != null)
         {
             // Open Settings Menu
             // Display Canvas, Freeze Time, Show Cursor, Disable Lookaround
-            if (SettingsCanvas.activeSelf)
+            if (!SettingsCanvas.activeSelf)
             {
                 SettingsCanvas.SetActive(true);
                 Time.timeScale = 0;
 
                 Cursor.lockState = CursorLockMode.None;
                 Cursor.visible = true;
-                playerInput.actions.FindActionMap("Gameplay").FindAction("CameraLook").Disable();
+                cameraLook.Disable();
+                Debug.Log("Settings Menu Opened");
             }
             // Close Settings Menu
             // Hide Canvas, Resume Time, Hide Cursor, Enable Lookaround
@@ -47,8 +52,37 @@ public class SettingsMenu : MonoBehaviour
 
                 Cursor.lockState = CursorLockMode.Locked;
                 Cursor.visible = false;
-                playerInput.actions.FindActionMap("Gameplay").FindAction("CameraLook").Enable();
+                cameraLook.Enable();
+                Debug.Log("Settings Menu Closed");
             }
+        }
+    }
+
+    public void ToggleFullscreen()
+    {
+        if (fullscreenToggle.isOn)
+        {
+            Screen.fullScreen = true;
+            Debug.Log("Fullscreen Enabled");
+        }
+        else
+        {
+            Screen.fullScreen = false;
+            Debug.Log("Fullscreen Disabled");
+        }
+    }
+
+    public void ToggleVsync()
+    {
+        if(vsyncToggle.isOn)
+        {
+            QualitySettings.vSyncCount = 1;
+            Debug.Log("Vsync Enabled");
+        }
+        else
+        {
+            QualitySettings.vSyncCount = 0;
+            Debug.Log("Vsync Disabled");
         }
     }
 }
