db609225b2e55683fcc915a27456b4ebfd430b55
Lin33735 <161744729+Lin33735@users.noreply.github.com>
2024-11-07 11:03:17 -0500

    Move Leaper Towards Player - 11/7

---
+++ Assets/Scripts/Entities/Enemies/Leaper/States/LeaperAttackState.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+public class LeaperAttackState : EnemyBaseState
+{
+    private Leaper leaper;
+
+    private Vector3 destination;
+
+    private float timer;
+
+    public LeaperAttackState(Leaper enemy) : base(enemy)
+    {
+        leaper = enemy;
+    }
+
+    public void SetLungeDestination(Vector3 dest)
+    {
+        destination = dest;
+    }
+
+    public override void OnEnter()
+    {
+        leaper.DefaultTransitionToAnimation("FlatMovement");
+
+        leaper.SetSpeedModifier(2f);
+
+        SetLungeDestination(leaper.Target.transform.position);
+
+        timer = 0;
+    }
+
+    public override void OnExit()
+    {
+        
+    }
+
+    public override void Update()
+    {
+        CheckForHits();
+        timer += Time.deltaTime;
+        if (timer > leaper.LungeDuration)
+        {
+            leaper.ChangeState(leaper.EntityEmptyState);
+            leaper.ChangeState(leaper.LeaperAttackState);
+            return;
+        }
+    }
+
+    public override void FixedUpdate()
+    {
+        Vector3 dir = (destination - leaper.transform.position).normalized;
+
+        leaper.Move(dir);
+    }
+
+    private void CheckForHits()
+    {
+
+    }
+}
