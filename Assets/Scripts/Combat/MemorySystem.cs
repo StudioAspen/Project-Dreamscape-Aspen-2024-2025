f15002f9b553fb6bef5388c8ef39e87feefbbd74
BenicioLadines <benicioladines814@gmail.com>
2024-11-08 16:46:22 -0500

    memory meter logic is in but not working yet

--- Assets/Scripts/Combat/MemorySystem.cs
+++ Assets/Scripts/Combat/MemorySystem.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
@@ -11,8 +12,29 @@ public class MemorySystem : MonoBehaviour
 {
     [SerializeField, Self] private Player player;
     [SerializeField]private int barMaximum; //amount of memory points to fill the memory bar
-    Dictionary<Type, int> memories = new Dictionary<Type, int>(); //dictionary so we can add new memory types whenever
+    Dictionary<Type, int> memories = new Dictionary<Type, int>(); //dictionary so we can add new memory types whenever, tied to enemy class ooooo
+    [SerializeField]private RectTransform memoryBarTransform;
+    private List<RectTransform> memoryTransforms = new List<RectTransform>();
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            
+        }
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+
+        }
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+
+        }
+    }
+    private void OnValidate()
+    {
+        this.ValidateRefs();
+    }
 
     private void OnEnable()
     {
@@ -35,7 +57,7 @@ public class MemorySystem : MonoBehaviour
         {
             memories[type] = memoryGained;
         }
-        
+        UpdateDebugMeter();
 
         if (MemoryIsFull())
         {
@@ -68,5 +90,31 @@ public class MemorySystem : MonoBehaviour
         Type memoryType = memories.Max(x => x.Key); //idk how this line works :p
         memories.Clear();
         return memoryType;
+    }
+
+    private void UpdateDebugMeter()
+    {
+        float maxWidth = memoryBarTransform.sizeDelta.x;
+        float height = memoryBarTransform.sizeDelta.y;
+
+        int i = 0;
+        float meterOffset = 0f;
+        foreach(Type memory in memories.Keys)
+        {
+            if( i+1 > memoryTransforms.Count)
+            {
+                memoryTransforms.Add(Instantiate(memoryBarTransform.gameObject, transform).GetComponent<RectTransform>());
+            }
+            float percentOfMeter = (float)memories[memory] / barMaximum;
+            Vector2 targetSize = new Vector2(maxWidth * percentOfMeter , height);
+            if (i > 0)
+            {
+                memoryTransforms[i].localPosition = new Vector2(memoryTransforms[i].localPosition.x, meterOffset);
+            }
+            memoryTransforms[i].sizeDelta = Vector2.Lerp(memoryTransforms[i].sizeDelta, targetSize, 10f * Time.deltaTime);
+            meterOffset += memoryTransforms[i].sizeDelta.x + memoryTransforms[i].localPosition.x;
+            i++;
+            
+        }
     }
 }
