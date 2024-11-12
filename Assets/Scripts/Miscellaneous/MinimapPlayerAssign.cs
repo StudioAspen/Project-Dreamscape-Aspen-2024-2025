8dcaf98078a36573343b99de1b25853c81045370
AFaraazK <92804285+ZaarafA@users.noreply.github.com>
2024-11-12 14:32:42 -0500

    Assignment Bug Fix

--- Assets/Scripts/Miscellaneous/MinimapPlayerAssign.cs
+++ Assets/Scripts/Miscellaneous/MinimapPlayerAssign.cs
@@ -2,14 +2,20 @@ using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using KBCore.Refs;
 
 public class MinimapPlayerAssign : MonoBehaviour
 {
-    [SerializeField] private Transform Player;
+    [SerializeField, Scene] private Player _player;
     [SerializeField] private Camera m_Camera;
     void Awake()
     {
-        m_Camera.GetComponent<CinemachineVirtualCamera>().Follow = Player.transform;
+        m_Camera.GetComponent<CinemachineVirtualCamera>().Follow = _player.transform;
+    }
+
+    private void OnValidate()
+    {
+        this.ValidateRefs();
     }
 
 }
