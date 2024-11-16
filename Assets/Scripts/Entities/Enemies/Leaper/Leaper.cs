8c352b6867c6fd0f013b1b31d06b2c77e35e3469
River! <123593150+blakestar67@users.noreply.github.com>
2024-11-15 19:11:38 -0500

    Brought changes from dev 2

--- Assets/Scripts/Entities/Enemies/Leaper/Leaper.cs
+++ Assets/Scripts/Entities/Enemies/Leaper/Leaper.cs
@@ -24,7 +24,7 @@ public class Leaper : Enemy
     #region States
 
     // public LeaperAttackState LeaperAttackState{ get; private set; }
-    // public LeaperHopState LeaperHopState{ get; private set; }
+    public LeaperHopState LeaperHopState{ get; private set; }
     // public LeaperChaseState LeaperChaseState{ get; private set; }
 
     #endregion
@@ -38,6 +38,7 @@ public class Leaper : Enemy
     {
         base.OnOnEnable();
         // set start state
+        SetStartState(LeaperHopState);
     }
 
     protected override void OnOnDisable()
@@ -74,7 +75,7 @@ public class Leaper : Enemy
         // add more as they get created
 
         // LeaperAttackState = new LeaperAttackState(this);
-        // LeaperHopState = new LeaperHopState(this);
+        LeaperHopState = new LeaperHopState(this);
         // LeaperChaseState = new LeaperChaseState(this);
 
 
