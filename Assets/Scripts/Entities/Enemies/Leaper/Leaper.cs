818e468ffdd5c2c7535b06a6485391dd23edf77f
Lin33735 <161744729+Lin33735@users.noreply.github.com>
2024-11-20 01:44:12 -0500

    Added Johns Leaper Tween

--- Assets/Scripts/Entities/Enemies/Leaper/Leaper.cs
+++ Assets/Scripts/Entities/Enemies/Leaper/Leaper.cs
@@ -1,15 +1,16 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Leaper : Enemy
-{   
+{
     // variables for attack go here 
     // follower has a couple of them already but I dont think leaper needs all of them
     [field: Header("Leaper: Attack Settings")]
     [field: SerializeField] public float AttackRange { get; private set; } = 1f;
     // reminder to change the value bellow if you need to
-    [field: SerializeField] public Vector2Int AttackDamageRange { get; private set; } = new Vector2Int(10,15);
+    [field: SerializeField] public Vector2Int AttackDamageRange { get; private set; } = new Vector2Int(10, 15);
     [field: SerializeField] public float LungeDuration { get; private set; } = 2f;
     [field: SerializeField] public float HitBoxRadius { get; private set; } = 2f;
     [field: SerializeField] public GameObject HitBoxLocation { get; private set; }
@@ -20,10 +21,10 @@ public class Leaper : Enemy
     // add as they get created
     #region States
 
-    public LeaperAttackState LeaperAttackState{ get; private set; }
+    public LeaperAttackState LeaperAttackState { get; private set; }
     // public LeaperHopState LeaperHopState{ get; private set; }
     // public LeaperChaseState LeaperChaseState{ get; private set; }
-    
+
     #endregion
 
     protected override void OnAwake()
@@ -51,7 +52,7 @@ public class Leaper : Enemy
     {
         base.OnStart();
         SetDefaultState(LeaperAttackState);
-    } 
+    }
 
     protected override void OnUpdate()
     {
@@ -85,7 +86,7 @@ public class Leaper : Enemy
     }
     public void CheckForHits()
     {
-        Collider[] hitColliders = Physics.OverlapSphere(HitBoxLocation.transform.position, HitBoxRadius,HitLayer);
+        Collider[] hitColliders = Physics.OverlapSphere(HitBoxLocation.transform.position, HitBoxRadius, HitLayer);
 
         foreach (var hitCollider in hitColliders)
         {
@@ -102,6 +103,19 @@ public class Leaper : Enemy
             }
         }
     }
+
+    public Tween TweenLeap(Vector3 leapDestination, float leapDuration, float leapHeight)
+    {
+        Vector3 startPoint = RigidBody.position;
+        Vector3 endPoint = leapDestination;
+        Vector3 midPoint = (startPoint + endPoint) / 2;
+        midPoint.y += leapHeight;
+        Vector3[] path = { startPoint, midPoint, endPoint };
+        return RigidBody.DOPath(path, leapDuration, PathType.CatmullRom).SetEase(Ease.Linear);
+    }
+
+
+
 
 
 }
