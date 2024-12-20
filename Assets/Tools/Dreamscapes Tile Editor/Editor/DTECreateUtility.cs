d14745d4a4c90dced3c5e1e917ff5fd9d71c0528
Mikyle <mikylemosquera@gmail.com>
2024-12-20 15:45:59 -0800

    Implemented assets and touched up Dreamscape Tile Editor Tool

--- Assets/Tools/Dreamscapes Tile Editor/Editor/DTECreateUtility.cs
+++ Assets/Tools/Dreamscapes Tile Editor/Editor/DTECreateUtility.cs
@@ -4,7 +4,7 @@ using UnityEditor;
 using UnityEditor.SceneManagement;
 using UnityEngine;
 
-namespace Dreamscapes.TileEditor
+namespace Dreamscape.TileEditor
 {
 	///-/////////////////////////////////////////////////////////////////////////
 	///
