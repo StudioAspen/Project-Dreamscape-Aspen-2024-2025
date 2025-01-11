39ef50fb1a32b7b7a018c7d309d7dce0f3cdb4b9
Samson Wu <78169553+samsonahh@users.noreply.github.com>
2025-01-10 21:45:58 -0500

    Animated Event Selection UI

--- Assets/Scripts/World/Event/EventSelectUI.cs
+++ Assets/Scripts/World/Event/EventSelectUI.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.Linq;
 using System;
+using DG.Tweening;
+using TMPro;
 
 public class EventSelectUI : MonoBehaviour
 {
@@ -11,13 +13,35 @@ public class EventSelectUI : MonoBehaviour
     private Image panel;
 
     [Header("References")]
+    [SerializeField] private Image background;
+    [SerializeField] private TMP_Text titleText;
     [SerializeField] private List<EventCardUI> eventCards;
+
+    [Header("On Enter Config")]
+    [SerializeField] private float backgroundFadeInDuration = 0.5f;
+    [SerializeField] private float flipDurationBetweenCards = 0.25f;
+    [SerializeField] private float enterCardsFlipDuration = 0.25f;
+    [SerializeField] private float cardDealDuration = 0.25f;
+
+    [Header("On Exit Config")]
+    [SerializeField] private float selectedCardCenteringDuration = 0.5f;
+    [SerializeField] private float selectedCardViewingDuration = 1f;
+    [SerializeField] private float cardSlideDownDuration = 0.5f;
+    [SerializeField] private float nonSelectedCardsExitFlipDuration = 0.25f;
+    [SerializeField] private float selectedCardExitFlipDuration = 0.25f;
+    private float backgroundFadeOutDuration => selectedCardExitFlipDuration + cardSlideDownDuration;
+
+    private Color backgroundStartingColor;
+    private Color titleTextStartingColor;
 
     private void Awake()
     {
         gameManager = FindObjectOfType<GameManager>();
         eventManager = FindObjectOfType<EventManager>();
         panel = GetComponent<Image>();
+
+        backgroundStartingColor = background.color;
+        titleTextStartingColor = titleText.color;
 
         gameManager.OnGameStateChanged += GameManager_OnGameStateChanged;
     }
@@ -43,17 +67,76 @@ public class EventSelectUI : MonoBehaviour
         gameObject.SetActive(true);
 
         AssignRandomEventsToCards();
+        
+        titleText.color = titleTextStartingColor;
 
-        EnableCards();
+        // Kill all previous tweens on the background and return it to its starting state
+        background.DOKill();
+        background.color = Color.clear;
+
+        // Fade in background
+        background.DOColor(backgroundStartingColor, backgroundFadeInDuration).SetUpdate(true).SetEase(Ease.InCubic);
+        
+        // Play the flipping cards animation
+        PlayStartAnimation();
     }
 
     public void Disable()
     {
-        DisableCards();
+        DisableCardButtons();
+
+        foreach(EventCardUI card in eventCards)
+        {
+            card.InstantlyFlipCard(false);
+        }
 
         gameObject.SetActive(false);
     }
 
+    /// <summary>
+    /// Plays the start animation by flipping event cards one after the other and enabling the cards at the end.
+    /// </summary>
+    private void PlayStartAnimation()
+    {
+        // Kill all previous tweens on the event cards and return them to their starting states
+        for (int i = 0; i < eventCards.Count; i++)
+        {
+            DOTween.Kill(eventCards[i]);
+            eventCards[i].ResetCard();
+        }
+
+        Vector3 bottomLeftPositionOffScreen = new Vector3(-Camera.main.pixelWidth, -Camera.main.pixelHeight, 0);
+
+        for (int i = 0; i < eventCards.Count; i++)
+        {
+            // Move the cards off screen
+            eventCards[i].transform.position = bottomLeftPositionOffScreen;
+
+            int localIndex = i;
+
+            if (i == eventCards.Count - 1)
+            {
+                PlayCardAnimationByIndex(localIndex, EnableCardButtons);
+                break;
+            }
+
+            PlayCardAnimationByIndex(localIndex);
+        }
+
+        void PlayCardAnimationByIndex(int index, Action onCompleteCallback = null)
+        {
+            DOVirtual.DelayedCall(index * flipDurationBetweenCards, () =>
+            {
+                eventCards[index].MoveToStartingPosition(cardDealDuration, Ease.OutCubic, () => {
+                    eventCards[index].FlipCard(enterCardsFlipDuration, true, onCompleteCallback);
+                });
+            }).SetId(eventCards[index]);
+        }
+    }
+
+    /// <summary>
+    /// Assigns random events to each event card.
+    /// </summary>
     private void AssignRandomEventsToCards()
     {
         List<Type> potentialEvents = new List<Type>(eventManager.EventsDictionary.Keys);
@@ -70,19 +153,75 @@ public class EventSelectUI : MonoBehaviour
         }
     }
 
-    private void EnableCards()
+    private void Card_OnCardClicked(EventCardUI clickedCard)
+    {
+        DisableCardButtons();
+
+        PlayExitAnimation(clickedCard);
+    }
+
+    /// <summary>
+    /// Plays the exit animation when a card is clicked.
+    /// </summary>
+    /// <param name="clickedCard">The clicked card.</param>
+    private void PlayExitAnimation(EventCardUI clickedCard)
+    {
+        foreach (EventCardUI card in eventCards)
+        {
+            if (card == clickedCard) continue;
+
+            // Flip and slide down the non-selected cards
+            card.FlipCard(nonSelectedCardsExitFlipDuration, false, () =>
+            {
+                card.transform.DOMoveY(-Camera.main.pixelHeight, cardSlideDownDuration).SetUpdate(true).SetEase(Ease.InOutBack);
+            });
+        }
+
+        // Fade the title away
+        titleText.DOColor(Color.clear, cardSlideDownDuration).SetUpdate(true);
+
+        // Center and scale the selected card and then slide it down after a delay
+        DOVirtual.DelayedCall(cardSlideDownDuration, () =>
+        {
+            clickedCard.transform.DOScale(1.2f, selectedCardCenteringDuration).SetEase(Ease.OutBack).SetUpdate(true); // Scale it
+
+            Vector3 screenCenter = new Vector3(Camera.main.pixelWidth / 2, Camera.main.pixelHeight / 2, 0);
+            clickedCard.transform.DOMove(screenCenter, selectedCardCenteringDuration).SetUpdate(true).SetEase(Ease.OutQuint).OnComplete(() => // Center it
+            {
+                DOVirtual.DelayedCall(selectedCardViewingDuration, () => // Wait for a bit to let the player read the card
+                {
+                    background.DOColor(Color.clear, backgroundFadeOutDuration).SetUpdate(true).SetEase(Ease.InCubic); // Fade out the background
+
+                    clickedCard.FlipCard(selectedCardExitFlipDuration, false, () => // Flip it
+                    {
+                        // Slide it down
+                        clickedCard.transform.DOMoveY(-Camera.main.pixelHeight, cardSlideDownDuration).SetUpdate(true).SetEase(Ease.InOutBack).OnComplete(() =>
+                        {
+                            eventManager.ChangeEvent(clickedCard.CurrentEventType); // Change the event
+                        });
+                    });
+                });
+            });
+        });
+    }
+
+    private void EnableCardButtons()
     {
         foreach (EventCardUI card in eventCards)
         {
             card.EnableButton();
+
+            card.OnCardClicked += Card_OnCardClicked;
         }
     }
 
-    private void DisableCards()
+    private void DisableCardButtons()
     {
         foreach (EventCardUI card in eventCards)
         {
             card.DisableButton();
+
+            card.OnCardClicked -= Card_OnCardClicked;
         }
     }
 }
