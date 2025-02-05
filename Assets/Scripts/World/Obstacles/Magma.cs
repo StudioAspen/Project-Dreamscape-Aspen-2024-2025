8d2bd47b83c10b08200575a8c46d7297e724d09d
Samson Wu <78169553+samsonahh@users.noreply.github.com>
2025-02-05 02:05:40 -0500

    Lava biome obstacles: Geyser and Magma pool (#172)
    * Geyser finished
    
    * Magma pool

---
+++ Assets/Scripts/World/Obstacles/Magma.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Magma : MonoBehaviour
+{
+    [SerializeField] private BurnStatusEffectSO burnStatusEffect;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        EntityStatusEffector.TryApplyStatusEffect(other.gameObject, burnStatusEffect, gameObject);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        BurnStatusEffectSO currentBurnStatus = EntityStatusEffector.TryGetStatusEffect<BurnStatusEffectSO>(other.gameObject);
+        if (currentBurnStatus == null)
+        {
+            EntityStatusEffector.TryApplyStatusEffect(other.gameObject, burnStatusEffect, gameObject);
+        }
+        else
+        {
+            currentBurnStatus.OverrideTicks(burnStatusEffect.Ticks);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        BurnStatusEffectSO currentBurnStatus = EntityStatusEffector.TryGetStatusEffect<BurnStatusEffectSO>(other.gameObject);
+        if (currentBurnStatus == null)
+        {
+            EntityStatusEffector.TryApplyStatusEffect(other.gameObject, burnStatusEffect, gameObject);
+        }
+        else
+        {
+            currentBurnStatus.OverrideTicks(burnStatusEffect.Ticks);
+        }
+    }
+}
