b1f9fcc376b0936a2d392e60965c90604bba1af4
Rafid Zia <rafidizia@gmail.com>
2025-02-07 00:48:32 -0500

    added levelsystem_onEXPadded method, added functionality

--- Assets/Scripts/Combat/ChainingSystem.cs
+++ Assets/Scripts/Combat/ChainingSystem.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ChainingSystem : MonoBehaviour
 {
     private PlayerCombat playerCombat;
+    private LevelSystem levelSystem;
 
     [Header("Settings")]
     [SerializeField] private float timeBetween = 1f;
+    [SerializeField] private int X = 12;
+    [SerializeField] private int Y = 18;
+    [SerializeField] private int Z = 24;
+    [SerializeField] private int A = 30;
     private float timer;
 
     public int ChainCount { get; private set; }
@@ -16,6 +22,7 @@ public class ChainingSystem : MonoBehaviour
     private void Awake()
     {
         playerCombat = GetComponent<PlayerCombat>();
+        levelSystem = GetComponent<LevelSystem>();
     }
 
     private void Start()
@@ -66,5 +73,32 @@ public class ChainingSystem : MonoBehaviour
     {
         ChainCount = 0;
         timer = 0;
+    }
+
+    private void LevelSystem_OnEXPAdded(int addedAmount)
+    {
+        int expBoost = 0;
+
+        if (ChainCount > 6 && ChainCount < X)
+        {
+            expBoost = Mathf.RoundToInt(addedAmount * 0.25f);
+        }
+        else if (ChainCount >= X && ChainCount < Y)
+        {
+            expBoost = Mathf.RoundToInt(addedAmount * 0.5f);
+        }
+        else if (ChainCount >= Y && ChainCount < Z)
+        {
+            expBoost = Mathf.RoundToInt(addedAmount * 0.75f);
+        }
+        else if (ChainCount >= Z && ChainCount < A)
+        {
+            expBoost = Mathf.RoundToInt(addedAmount * 2f);
+        }
+
+        if (expBoost > 0)
+        {
+            levelSystem.AddEXP(expBoost);
+        }
     }
 }
