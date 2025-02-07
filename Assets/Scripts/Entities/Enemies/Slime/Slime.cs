918d62bf5f158a089e0f75395663f7e2a9222f25
MarioV <mvaquero143@gmail.com>
2025-02-07 18:30:22 -0500

    edits made

--- Assets/Scripts/Entities/Enemies/Slime/Slime.cs
+++ Assets/Scripts/Entities/Enemies/Slime/Slime.cs
@@ -9,7 +9,7 @@ public class Slime : Enemy
     [field: SerializeField] public float DetectionConeHalfAngle { get; private set; } = 40f;
 
     [field: SerializeField] public bool isSplit { get; private set; } = false; 
-
+    
 
     #region States
     public SlimeIdleState SlimeIdleState {get; private set;}
@@ -17,6 +17,11 @@ public class Slime : Enemy
     public SlimeAttackState SlimeAttackState {get; private set;}
     public SlimeSplitState SlimeSplitState {get; private set;}
     public SlimeGrowthState SlimeGrowthState {get; private set;}
+
+    // private void Init(Entity entity)
+    // {
+    //     slime = entity as Slime;
+    // }
 
     private protected override void InitializeStates()
     {
@@ -32,9 +37,12 @@ public class Slime : Enemy
 
     #endregion
 
+    
+
     private protected override void OnAwake()
     {
         base.OnAwake();
+
     }
 
     private protected override void OnOnEnable()
@@ -54,6 +62,8 @@ public class Slime : Enemy
         base.OnStart();
 
         SetDefaultState(SlimeSplitState);  
+
+        Debug.Log(Spawner);
     }
 
     private protected override void OnUpdate()
@@ -84,12 +94,12 @@ public class Slime : Enemy
     }
 
 
-    /// <summary>
-    /// Checks for collisions with enemy entities and applies damage if a collision occurs.
-    /// Pass in a reference to a list of hit entities to prevent multiple hits on the same entity.
-    /// </summary>
-    /// <param name="damagePercent">The percentage damage to apply.</param>
-    /// <param name="hitEntities">A reference to the list of hit entities.</param>
+    // <summary>
+    // Checks for collisions with enemy entities and applies damage if a collision occurs.
+    // Pass in a reference to a list of hit entities to prevent multiple hits on the same entity.
+    // </summary>
+    // <param name="damagePercent">The percentage damage to apply.</param>
+    // <param name="hitEntities">A reference to the list of hit entities.</param>
     // public void CheckCollisions(float damagePercent, ref List<Entity> hitEntities)
     // {
     //     // List<Collider> hits = GetCustomCollisionHits(LeaperAttackState.LeapAttackLayerMask);
