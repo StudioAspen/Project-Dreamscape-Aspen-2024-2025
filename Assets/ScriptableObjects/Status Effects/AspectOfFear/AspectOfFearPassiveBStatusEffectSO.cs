eb46c183d8c1bfa8e5d61bf373dfe07410d9597b
Kyle Yang <kyleyang2020@gmail.com>
2025-02-18 17:35:04 -0500

    created new folder and script for fear passive b

---
+++ Assets/ScriptableObjects/Status Effects/AspectOfFear/AspectOfFearPassiveBStatusEffectSO.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "Status Effect/Aspect of Fear/Passive B")]
+public class AspectOfFearPassiveBStatusEffectSO : StatusEffectSO
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
