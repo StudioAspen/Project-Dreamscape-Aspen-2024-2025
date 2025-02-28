15e55c1fdc42a7ccd18d03bf16d40bcfd11d2b3c
Kyle Yang <112425182+kyleyang2020@users.noreply.github.com>
2025-02-28 18:03:14 -0500

    moved files/SO, fixed token challenge

--- Assets/Scripts/World/Progression/Quests/VisitEveryLandTokenChallenge.cs
+++ Assets/Scripts/World/Progression/Quests/VisitEveryLandTokenChallenge.cs
@@ -6,43 +6,37 @@ using static UnityEditor.Experimental.GraphView.GraphView;
 [CreateAssetMenu(fileName = "Achieve Chain Count Progression Quest", menuName = "World/Progression Quest/Visit Every Land During Wave Token Challenge")]
 public class VisitEveryLandTokenChallenge : ProgressionQuestSO
 {
-    private ChainingSystem chainingSystem;
-
-    [field: Header("Config")]
-    [field: SerializeField] public int ChainGoal { get; private set; } = 15;
+    private int currentLandVisited;
+    private int landNeeded;
 
     private Dictionary<Vector2Int, GameObject> visitIndicatorsDictionary = new Dictionary<Vector2Int, GameObject>();
 
     private protected WorldManager worldManager;
 
-    private List<Player> players = new List<Player>();
+    private Player player;
 
     private protected override void OnActivated()
     {
-        // Find all players and if there are none, clear the event
-        players = GameObject.FindObjectsByType<Player>(FindObjectsSortMode.None).ToList();
-        if (players == null)
-        {
-            CleanUp();
-            return;
-        }
-        if (players.Count <= 0)
-        {
-            CleanUp();
-            return;
-        }
-
-        chainingSystem = FindObjectOfType<ChainingSystem>();
-        if (chainingSystem == null)
+        // find all players and if there are none, clear the event
+        player = GameObject.FindObjectOfType<Player>();
+        if (player == null)
         {
             CleanUp();
             return;
         }
 
         // create visit indicators on all lands and set ChainGoal
+        worldManager = GameObject.FindObjectOfType<WorldManager>();
+
+        if (worldManager == null)
+        {
+            CleanUp();
+            return;
+        }
+
         foreach (LandManager land in worldManager.SpawnedLands.Values)
         {
-            ChainGoal++;
+            landNeeded++;
             visitIndicatorsDictionary.Add(land.GridPosition,
                 CustomDebug.InstantiateTemporarySphere(land.transform.position + 5f * Vector3.up, 10f, Mathf.Infinity, new Color(1, 0, 0, 0.5f)));
         }
@@ -55,25 +49,21 @@ public class VisitEveryLandTokenChallenge : ProgressionQuestSO
 
     private protected override void OnUpdate()
     {
-        if (chainingSystem == null) return; ;
-
-        if (chainingSystem.ChainCount >= ChainGoal)
+        if(currentLandVisited == landNeeded)
         {
-            Complete();
+            this.Complete();
             return;
         }
 
-        // Check if any player is on a land and remove the visit indicator and add to chain count
-        for (int i = 0; i < players.Count; i++)
-        {
-            Vector2Int playerGridPosition = worldManager.GetGridPosition(players[i].transform.position);
 
-            if (visitIndicatorsDictionary.ContainsKey(playerGridPosition))
-            {
-                chainingSystem.AddChain();
-                GameObject.Destroy(visitIndicatorsDictionary[playerGridPosition]);
-                visitIndicatorsDictionary.Remove(playerGridPosition);
-            }
+        // Check if any player is on a land and remove the visit indicator and add to chain count
+        Vector2Int playerGridPosition = worldManager.GetGridPosition(player.transform.position);
+
+        if (visitIndicatorsDictionary.ContainsKey(playerGridPosition))
+        {
+            currentLandVisited++;
+            GameObject.Destroy(visitIndicatorsDictionary[playerGridPosition]);
+            visitIndicatorsDictionary.Remove(playerGridPosition);
         }
     }
 }
