065829ea0c2d6a56f2c43984e65b42e9d6b30622
CANCANNEED23 <114161096+CANCANNEED23@users.noreply.github.com>
2025-02-28 11:47:47 -0500

    created the asset and script for task
    The script is called CompleteWaveUnderXLandCountTimeQuestSO.cs and the asset is created, but not tested b/c I don't know how to.

---
+++ Assets/Scripts/World/Progression/Quests/CompleteWaveUnderXLandCountTimeQuestSO.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Complete Wave Under X * LandCount Time Quest", menuName = "World/Progression Quest/Complete Wave Under X LandCount Time")]
+public class CompleteWaveUnderXLandCountTimeQuestSO : ProgressionQuestSO
+{
+    private EnemySpawner[] enemySpawners;
+    private WorldManager worldManager;
+
+    [field: Header("Config")]
+    [field: SerializeField] public float TimeMultiplier { get; private set; } = 60f;
+
+    private float requiredTime;
+
+    private protected override void OnActivated()
+    {
+        worldManager = FindObjectOfType<WorldManager>();
+        enemySpawners = FindObjectsOfType<EnemySpawner>();
+
+        int landCount = worldManager.SpawnedLands.Count;
+        requiredTime = TimeMultiplier * landCount;
+
+        foreach (var spawner in enemySpawners)
+        {
+            spawner.OnSpawnerDepleted += TrackWaveCompletion;
+        }
+    }
+
+    private protected override void OnCleanUp()
+    {
+        foreach (var spawner in enemySpawners)
+        {
+            spawner.OnSpawnerDepleted -= TrackWaveCompletion;
+        }
+    }
+
+    private protected override void OnUpdate()
+    {
+    }
+
+    private void TrackWaveCompletion()
+    {
+        float waveCompletionTime = Time.timeSinceLevelLoad; 
+
+        if (waveCompletionTime <= requiredTime)
+        {
+            Complete();
+        }
+    }
+}
