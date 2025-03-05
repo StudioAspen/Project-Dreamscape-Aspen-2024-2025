a4c089f014009c89f8082c78950c46ccd9530db1
MarioV <mvaquero143@gmail.com>
2025-03-05 01:52:45 -0500

    script to display
    untested at the moment but should display text or return a log

---
+++ Assets/Scripts/UI/StatPanel.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class StatPanel : MonoBehaviour 
+{
+    [SerializeField] private Stat stat;
+    [SerializeField] private TextMeshPro StatPanelText;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateDisplayText();
+    }
+
+    private void UpdateDisplayText()
+    {
+        // for debugging
+        if (stat == null || StatPanelText == null)
+        {
+            Debug.Log("Stat: " + stat + "StatPanelText: " + StatPanelText);
+            return;
+        }
+
+        float baseValue = stat.BaseValue;
+        float flatIncrease = stat.GetTotalFlatIncreass();
+        float totalMultipliers = stat.GetTotalMultiplier();
+
+
+        string displayText = $"Base Value: {baseValue}\n" +
+                             $"Total Multipliers: {string.Join(", ", totalMultipliers)}\n" +
+                             $"Flat Increase: {flatIncrease}";
+
+        StatPanelText.text = displayText;
+    }
+}
