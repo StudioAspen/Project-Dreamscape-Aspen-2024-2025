7256245c90553e542e076f005f892d224e62cc8b
Joshua Nunez <joshover23@gmail.com>
2025-03-05 10:23:08 -0500

    minimap functionality
    inspector scaling with the maximized size seems to now work properly, manually scaled for now working on fix.

--- Assets/Scripts/Entities/Player/PlayerUI.cs
+++ Assets/Scripts/Entities/Player/PlayerUI.cs
@@ -2,8 +2,11 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Cinemachine;
 using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Rendering.Universal;
 using UnityEngine.UI;
 
 public class PlayerUI : MonoBehaviour
@@ -11,6 +14,8 @@ public class PlayerUI : MonoBehaviour
     private Player player;
     private AspectsManager aspectsManager;
     private MemorySystem memorySystem;
+    private GameManager gameManager; //state changes
+    private PlayerControls playerControls; //minimap keybind
 
     [Header("Health")]
     [SerializeField] private Slider healthBar;
@@ -25,12 +30,38 @@ public class PlayerUI : MonoBehaviour
     [SerializeField] private TMP_Text memoryText;
     private Dictionary<string, RectTransform> shardBarTransforms = new();
 
+    [Header("Minimap")]
+    [SerializeField] private RectTransform minimapTransform; //Component used for transforming 2d ui element.
+    private Vector2 normalSize;
+    private Vector2 normalPosition;
+    private Vector2 centeredPosition;
+
+    private bool isMaximized = false; //maximized is when component is activated.
+    [SerializeField] Vector2 maximizedSize = new Vector2(300, 300);
+    [SerializeField] int maximizedZoom = 80;
+    [Range(0.0f, 1.0f)][SerializeField] private float minimap_opacity = 0.75f;
+    private float originalZoom;
+
+    [SerializeField] private Camera minimapCamera; //Top down camera that follows player (keeps them at center).
+    private CinemachineVirtualCamera virtualCamera;
+    private Cinemachine3rdPersonFollow thirdPersonFollow;
+
+    [SerializeField] private RawImage mapTexture; //Image of floor for minimap.
+    private Mask mask;
+    private RawImage image;
+    private Transform border;
+
+   
+
+
+
     // Awake is safe here because UI Scene loads last
     private void Awake()
     {
         player = FindObjectOfType<Player>();
         aspectsManager = player.GetComponent<AspectsManager>();
         memorySystem = player.GetComponent<MemorySystem>();
+        gameManager = FindAnyObjectByType<GameManager>();
 
         player.OnEntityTakeDamage += Player_OnEntityTakeDamage;
         player.OnEntityHeal += Player_OnEntityHeal;
@@ -41,13 +72,35 @@ public class PlayerUI : MonoBehaviour
         memorySystem.OnShardAdded += MemorySystem_OnShardAdded;
         memorySystem.OnMemoryBarFull += MemorySystem_OnMemoryBarFull;
         memorySystem.OnMemoryAbilityActivated += MemorySystem_OnMemoryAbilityActivated;
+
+        gameManager.OnGameStateChanged += GameManager_OnGameStateChanged; //subscribe to event
     }
 
     private void Start()
     {
         UpdateHealthBar(player.CurrentHealth);
         UpdateAspectsIcons();
+
+        // Cache Components
+        normalSize = minimapTransform.sizeDelta;
+        normalPosition = minimapTransform.anchoredPosition;
+        originalZoom = minimapCamera.GetComponent<CinemachineVirtualCamera>().GetCinemachineComponent<Cinemachine3rdPersonFollow>().VerticalArmLength;
+
+        RectTransform canvasRT = GetComponentInParent<Canvas>().GetComponent<RectTransform>(); //*Unsure how system wants to be implemented but using Main Canvas Component instead of a New Child Canvas*
+        centeredPosition = new Vector2(-(canvasRT.rect.width / 2), canvasRT.rect.height / 2);
+
+        virtualCamera = minimapCamera.GetComponent<CinemachineVirtualCamera>();
+        thirdPersonFollow = virtualCamera.GetCinemachineComponent<Cinemachine3rdPersonFollow>();
+
+        mask = GetComponentInChildren<Mask>();
+        image = mask.GetComponentInChildren<RawImage>();
+        border = transform.Find("Minimap/Minimap Background");
+
+        playerControls = FindAnyObjectByType<GameInputManager>().PlayerControls;
+        playerControls.Gameplay.ToggleMinimap.performed += PlayerControls_OnToggleMinimapPerformed; //subscribe to event
+
     }
+
 
     private void OnDestroy()
     {
@@ -60,6 +113,9 @@ public class PlayerUI : MonoBehaviour
         memorySystem.OnShardAdded -= MemorySystem_OnShardAdded;
         memorySystem.OnMemoryBarFull -= MemorySystem_OnMemoryBarFull;
         memorySystem.OnMemoryAbilityActivated -= MemorySystem_OnMemoryAbilityActivated;
+
+        gameManager.OnGameStateChanged -= GameManager_OnGameStateChanged; //unsubscribe to event
+        playerControls.Gameplay.ToggleMinimap.performed -= PlayerControls_OnToggleMinimapPerformed; //unsubscribe to event
     }
 
     private void Player_OnEntityHeal(Entity entity, int healAmount)
@@ -176,5 +232,82 @@ public class PlayerUI : MonoBehaviour
         shardBarImage.color = memorySystem.ShardDictionary[shardType].Color;
 
         return shardBarTransform;
+    }
+
+
+    //MINIMAP
+    private void PlayerControls_OnToggleMinimapPerformed(InputAction.CallbackContext context)
+    {
+        ToggleMinimap();
+    }
+
+    public void ToggleMinimap()
+    {
+        // Disable the Toggle when the game isn't running
+        if ((gameManager.CurrentState != GameState.PLAYING) && !isMaximized)
+        {
+            return;
+        }
+
+        // Toggle Mask and Border
+        if (border != null)
+        {
+           border.gameObject.SetActive(isMaximized);
+        }
+
+        if (mask != null)
+        {
+            mask.enabled = !mask.enabled;
+            mask.GetComponent<Image>().enabled = !mask.GetComponent<Image>().enabled;
+        }
+
+        // Toggle Solid Color Background of Minimap
+        if (minimapCamera.TryGetComponent(out UniversalAdditionalCameraData cameraData))
+        {
+            cameraData.renderPostProcessing = !cameraData.renderPostProcessing;
+        }
+
+        // Map is Maximized --> Minimize map
+        if (isMaximized)
+        {
+            // Make Minimap smaller and move into corner
+            minimapTransform.sizeDelta = normalSize;
+            minimapTransform.anchoredPosition = normalPosition;
+            thirdPersonFollow.VerticalArmLength = originalZoom;
+
+            Color newColor = image.color;
+            newColor.a = 1.0f;
+            image.color = newColor;
+        }
+        // Map is Minimized --> Maximize Map
+        else
+        {
+            if (image != null)
+            {
+                Color newColor = image.color;
+                newColor.a = minimap_opacity;
+                image.color = newColor;
+            }
+
+            // Center Map and Increaes Size
+            minimapTransform.sizeDelta = maximizedSize;
+            minimapTransform.anchoredPosition = centeredPosition;
+
+            thirdPersonFollow.VerticalArmLength = maximizedZoom;
+        }
+
+        isMaximized = !isMaximized;
+    }
+
+    private void GameManager_OnGameStateChanged(GameState newState)
+    {
+        // If the game isn't running, hide minimap
+        if ((newState != GameState.PLAYING))
+        {
+            if (isMaximized)
+            {
+                ToggleMinimap();
+            }
+        }
     }
 }
