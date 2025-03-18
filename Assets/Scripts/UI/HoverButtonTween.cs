26a667bcba70a24ee899a1516fe8755e24b6996e
Samson Wu <samsonwu05@gmail.com>
2025-03-18 17:35:31 -0400

    Text button glow for clarity

--- Assets/Scripts/UI/HoverButtonTween.cs
+++ Assets/Scripts/UI/HoverButtonTween.cs
@@ -1,10 +1,11 @@
 using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class HoverButtonTween : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, ISelectHandler, IDeselectHandler
+public class HoverButtonTween : HoverUI
 {
     [Header("Config")]
     [SerializeField] private float hoverScale = 1.1f;
@@ -12,41 +13,27 @@ public class HoverButtonTween : MonoBehaviour, IPointerEnterHandler, IPointerExi
     [SerializeField] private Ease tweenEase = Ease.OutCubic;
 
     private Vector3 originalScale;
-    private bool isSelected;
 
     private void Start()
     {
         originalScale = transform.localScale;
     }
 
-    private void OnEnable()
+    private protected override void OnOnEnable()
     {
-        if(isSelected) OnSelect(null);
+        
     }
 
-    public void OnPointerEnter(PointerEventData eventData)
+    private protected override void OnOnSelected(BaseEventData eventData)
     {
-        OnSelect(eventData);
-    }
-
-    public void OnPointerExit(PointerEventData eventData)
-    {
-        OnDeselect(eventData);
-    }
-
-    public void OnSelect(BaseEventData eventData)
-    {
-        isSelected = true;
-
         transform.DOKill();
         transform.DOScale(originalScale * hoverScale, tweenDuration).SetEase(tweenEase).SetUpdate(true);
     }
 
-    public void OnDeselect(BaseEventData eventData)
+    private protected override void OnOnDeselected(BaseEventData eventData)
     {
-        isSelected = false;
-
         transform.DOKill();
         transform.DOScale(originalScale, tweenDuration).SetEase(tweenEase).SetUpdate(true);
     }
 }
+
---
+++ Assets/Scripts/UI/HoverTextGlow.cs
@@ -0,0 +1,47 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HoverTextGlow : HoverUI
+{
+    private TMP_Text text;
+
+    [Header("Config")]
+    [SerializeField] private Color glowColor = Color.white;
+    [SerializeField] private float glowPower = 1.5f;
+
+    private Material originalMaterial;
+    private Material glowMaterial;
+
+    private void Awake()
+    {
+        text = GetComponent<TMP_Text>();
+        if (text == null) text = GetComponentInChildren<TMP_Text>();
+        if(text == null)
+        {
+            Destroy(this);
+            return;
+        }
+        originalMaterial = text.fontMaterial;
+        glowMaterial = new Material(originalMaterial);
+        glowMaterial.EnableKeyword("GLOW_ON"); // Enable the glow keyword
+        glowMaterial.SetColor(ShaderUtilities.ID_GlowColor, glowColor);
+        glowMaterial.SetFloat(ShaderUtilities.ID_GlowPower, glowPower);
+    }
+
+    private protected override void OnOnEnable()
+    {
+        text.fontMaterial = originalMaterial;
+    }
+
+    private protected override void OnOnSelected(BaseEventData eventData)
+    {
+        text.fontMaterial = glowMaterial;
+    }
+
+    private protected override void OnOnDeselected(BaseEventData eventData)
+    {
+        text.fontMaterial = originalMaterial;
+    }
+}
+
