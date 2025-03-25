048a578404abe281637226a60cd2d4a86b02e62b
Samson Wu <78169553+samsonahh@users.noreply.github.com>
2025-03-25 08:40:21 -0400

    2x2 Start

--- Assets/Scripts/World/Event/Events/TutorialWorldEventSO.cs
+++ Assets/Scripts/World/Event/Events/TutorialWorldEventSO.cs
@@ -39,7 +39,7 @@ public class TutorialWorldEventSO : WorldEventSO
         hammer.OnWeaponHit += Hammer_OnWeaponHit;
         hammer.OnWeaponStartSwing += Hammer_OnWeaponStartSwing;
 
-        dummyInstance = Instantiate(DummyPrefab, 5f * Vector3.up, Quaternion.Euler(0f, 180f, 0f));
+        dummyInstance = Instantiate(DummyPrefab, new Vector3(worldManager.LandScale/2f, 5f, worldManager.LandScale/2f), Quaternion.Euler(0f, 180f, 0f));
 
         isFinished = false;
         afterFinishTimer = 0f;
