b77beacb9a48b3f950637f642421605a7a8c10a6
Evan Caplinger <tshiknn@gmail.com>
2025-03-27 18:06:17 -0700

    add music and spatialized audio

--- Assets/Scripts/Entities/Player/PlayerCombat.cs
+++ Assets/Scripts/Entities/Player/PlayerCombat.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7fc81bf44e803a9510d7c4acfa56b1bc76d562c197454ae929eacda1d6c57e25
-size 10522
+oid sha256:2d4c77fe3c2dfbc411e89464da62f796cb2e5188bbec58a46f7dc497ddee5489
+size 10389
