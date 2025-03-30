d985737b6d7a26d55042018f65a54776fa6ca966
ChillBeanJosh <joshover23@gmail.com>
2025-03-30 14:04:55 -0400

    setup
    - saving to work on another device

---
+++ Assets/Scripts/Abilities/SlimeLaunch.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Dreamscape.Abilities
+{
+    public class SlimeLaunch : CastedAbility, IPoolableObject
+    {
+        [Header("Settings")]
+        [SerializeField] private float slimeIndex = 1; //Index used to determine if slime can split
+        [SerializeField] private float playerSlimeLaunch = Mathf.PI / 4;
+        [SerializeField] private float deployedSlimeLaunch = Mathf.PI / 2;
+
+        //y(t) = 1/2 * At^2 + V_0 * t + H_0
+        //H_0 = "Player Height"
+        //V_0 = "Velocity"
+        //A = "Gravity (-9.8)
+        [SerializeField] private float playerHeight;
+        [SerializeField] private float slimeVelocity = 5f;
+        [SerializeField] private float projectileMotionHeight;
+
+        //Slime Object itself:
+        [SerializeField] private GameObject slimeObject;
+
+        private void Start()
+        {
+            playerHeight = casterEntity.CharacterController.height;
+        }
+
+        //Time.FixedDeltaTime 
+        private void FixedUpdate()
+        {
+            projectileMotionHeight = .5f * casterEntity.PhysicsConfig.Gravity * (Time.fixedDeltaTime) * (Time.fixedDeltaTime) + slimeVelocity * (Time.fixedDeltaTime) + playerHeight;
+
+        
+
+
+
+           
+        }
+
+
+
+
+
+        private protected override void OnSpawn()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private protected override void OnOnDisable()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+}
+
