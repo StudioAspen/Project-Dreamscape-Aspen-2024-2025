557a90d566246ddf7292838ef70eb5a6e3061b4b
MarioV <mvaquero143@gmail.com>
2025-04-04 01:50:04 -0400

    added new stats
    had issues with previous branch had to make new one

---
+++ Assets/Scripts/UI/StatPanel.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using Unity.VisualScripting;
+
+public class StatPanel : MonoBehaviour 
+{
+    private Player player;
+    
+    [SerializeField] private TMP_Text StatPanelText;
+
+    public List<Stat> statList;
+    public List<TMP_Text> DisplayText;
+
+    // Update is called once per frame
+    private void Awake()
+    {
+        player = FindObjectOfType<Player>().GetComponent<Player>();
+        statList.Add(player.MaxHealth);
+        statList.Add(player.StatusSpeedModifier);
+        statList.Add(player.DamageModifier);
+        statList.Add(player.LocalTimeScale);
+        statList.Add(player.SizeScale);
+        statList.Add(player.Defense);
+        statList.Add(player.DebuffApplyDurationMultiplier);
+        statList.Add(player.BuffApplyDurationMultiplier);
+
+        
+        
+    }
+
+    private void Start()
+    {
+        /*displayStats();*/
+    }
+
+    private void OnEnable()
+    {
+        /*For Some reason is called twice? and doubles up displayed values*/
+        displayStats();
+    }
+
+    private void displayStats()
+    {
+        for(int i = 0; i < statList.Count; i++)
+        {
+            /*Debug.Log("I displayed");*/
+            /*DisplayText[i].text = "";*/
+            /* Name of Stat, Current Stat Value,Base Value, totalmult, totalflatincrease*/
+            /*DisplayText[i].text = statList[i].DisplayName.ToString();*/
+            DisplayText[i].text += " ";
+            DisplayText[i].text += statList[i].GetIntValue().ToString();
+            DisplayText[i].text += " ";
+            DisplayText[i].text += statList[i].BaseValue.ToString();
+            DisplayText[i].text += " ";
+            DisplayText[i].text += statList[i].GetTotalMultiplier().ToString();
+            DisplayText[i].text += " ";
+            DisplayText[i].text += statList[i].GetTotalFlatIncreass().ToString();
+            
+        }
+    }
+}
